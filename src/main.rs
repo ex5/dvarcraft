0ac9f251605ace5c5712ced974a32156851fddfc
railla <dev.local@gmail.com>
2017-10-21 15:29:46 +0200

    gfx

---
+++ src/main.rs
@@ -0,0 +1,267 @@
+#[macro_use] extern crate gfx;
+extern crate env_logger;
+extern crate rand;
+extern crate sdl2;
+extern crate gfx_core;
+extern crate gfx_corell;
+extern crate gfx_device_gl;
+extern crate gfx_window_sdl;
+extern crate cgmath;
+extern crate image;
+extern crate find_folder;
+extern crate clock_ticks;
+
+mod gfx_text;
+mod support;
+mod textures;
+mod selection;
+mod quadtree;
+mod miners;
+mod tiles;
+
+use gfx::{Device, GraphicsPoolExt};
+use support::{BackbufferView, ColorFormat};
+use std::collections::HashSet;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+const SPRITE_WIDTH: f32 = 32.0;
+const SPRITE_VERTICES: [Vertex; 4] = [
+    Vertex { position: [-SPRITE_WIDTH,  SPRITE_WIDTH] },
+    Vertex { position: [ SPRITE_WIDTH,  SPRITE_WIDTH] },
+    Vertex { position: [-SPRITE_WIDTH, -SPRITE_WIDTH] },
+    Vertex { position: [ SPRITE_WIDTH, -SPRITE_WIDTH] },
+];
+
+const SPRITE_INDICES: [u16; 6] = [0, 1, 2, 1, 3, 2];
+
+gfx_defines!{
+    vertex Vertex {
+        position: [f32; 2] = "i_position",
+    }
+
+    // color format: 0xRRGGBBAA
+    vertex Instance {
+        translate: [f32; 2] = "a_Translate",
+        tex_id: u32 = "i_tex_id",
+        is_selected: f32 = "is_selected",
+    }
+
+    pipeline pipe {
+        vertex: gfx::VertexBuffer<Vertex> = (),
+        instance: gfx::InstanceBuffer<Instance> = (),
+        scale: gfx::Global<f32> = "u_Scale",
+        matrix: gfx::Global<[[f32; 4]; 4]> = "matrix",
+        tex: gfx::TextureSampler<[f32; 4]> = "tex",
+        out: gfx::BlendTarget<ColorFormat> = ("f_color", gfx::state::MASK_ALL, gfx::preset::blend::ALPHA),
+    }
+}
+
+fn fill_instances(instances: &mut [Instance], start_idx: usize, tiles: &Vec<&tiles::Tile>) {
+    for (i, tile) in tiles.iter().enumerate() {
+        instances[start_idx + i] = Instance {
+            translate: [tile.position.x, tile.position.y],
+            tex_id: tile.tex_id,
+            is_selected: match tile.is_selected { true => 0.5, false => 0.0 },
+        };
+    }
+ }
+
+const MAX_INSTANCE_COUNT: usize = 2048;
+
+pub struct App<B: gfx::Backend> {
+    running: bool,
+    zoom: f32,
+    viewport_w: f32,
+    viewport_h: f32,
+    views: Vec<BackbufferView<B::Resources>>,
+    pso: gfx::PipelineState<B::Resources, pipe::Meta>,
+    data: pipe::Data<B::Resources>,
+    slice: gfx::Slice<B::Resources>,
+    tiles: tiles::Tiles,
+    miners: miners::Miners,
+    instance_count: usize,
+    //upload: gfx::handle::Buffer<B::Resources, Instance>,
+    prev_buttons: HashSet<sdl2::mouse::MouseButton>,
+    selection: selection::Selection,
+    cur_tile: Option<usize>,
+}
+
+impl<B: gfx::Backend> support::Application<B> for App<B> {
+    fn new(device: &mut B::Device,
+           _: &mut gfx::queue::GraphicsQueue<B>,
+           backend: support::shade::Backend,
+           window_targets: support::WindowTargets<B::Resources>) -> Self
+    {
+        use gfx::traits::DeviceExt;
+        //use gfx_corell::factory::Factory;
+
+        let vs = support::shade::Source {
+            glsl_330: include_bytes!("shader/instancing_120.glslv"),
+            .. support::shade::Source::empty()
+        };
+        let fs = support::shade::Source {
+            glsl_330: include_bytes!("shader/instancing_120.glslf"),
+            .. support::shade::Source::empty()
+        };
+
+        let mut tiles = tiles::Tiles::new_layer_from_heightmap("heightmap_64.png", 2);
+        let mut miners = miners::Miners::new(10, &tiles);
+        let miners_count: usize = miners.miners.len();
+        let sprites_count: usize = tiles.tiles.len();
+        let instance_count = sprites_count + miners_count;
+        println!("Number of sprites: {}", instance_count);
+
+        let zoom = 1.0;
+        let (viewport_w, viewport_h) = (800.0, 600.0);
+        let ortho_matrix: cgmath::Matrix4<f32> = cgmath::ortho(
+            - viewport_w / 2.0 * zoom, viewport_w / 2.0 * zoom,
+            - viewport_h / 2.0 * zoom, viewport_h / 2.0 * zoom,
+            -1.0, 1.0);
+        let (quad_vertices, mut slice) = device
+            .create_vertex_buffer_with_slice(&SPRITE_VERTICES, &SPRITE_INDICES[..]);
+        let instances = device
+            .create_buffer(instance_count,
+                           gfx::buffer::Role::Vertex,
+                           gfx::memory::Usage::Data,
+                           gfx::TRANSFER_DST).unwrap();
+        App {
+            running: true,
+            zoom: zoom,
+            viewport_w: viewport_w,
+            viewport_h: viewport_h,
+            miners: miners,
+            tiles: tiles,
+            instance_count: instance_count,
+            slice: slice,
+            data: pipe::Data {
+                vertex: quad_vertices,
+                instance: instances,
+                scale: 1.0,
+                matrix: Into::<[[f32; 4]; 4]>::into(ortho_matrix),
+                // texture atlas
+                tex: (textures::load_textures(device), device.create_sampler_linear()),
+                out: window_targets.views[0].0.clone(),
+            },
+            pso: device.create_pipeline_simple(
+                vs.select(backend).unwrap(),
+                fs.select(backend).unwrap(),
+                pipe::new()
+                ).unwrap(),
+            //upload: upload,
+            views: window_targets.views,
+            prev_buttons: HashSet::new(),
+            selection: selection::Selection::new(),
+            cur_tile: None,
+        }
+    }
+
+    fn render(&mut self, device: &mut B::Device, (frame, sync): (gfx::Frame, &support::SyncPrimitives<B::Resources>),
+              pool: &mut gfx::GraphicsCommandPool<B>, queue: &mut gfx::queue::GraphicsQueue<B>)
+    {
+        use gfx::traits::DeviceExt;
+
+        let upload = device.create_upload_buffer(self.instance_count).unwrap();
+        {
+            let mut writer = device.write_mapping(&upload).unwrap();
+            fill_instances(&mut writer, 0, &self.tiles.get_tiles());
+            fill_instances(&mut writer, self.tiles.tiles.len(), &self.miners.get_tiles());
+        };
+
+        self.slice.instances = Some((self.instance_count as u32, 0));
+        let ortho_matrix: cgmath::Matrix4<f32> = cgmath::ortho(
+            - self.viewport_w / 2.0 * self.zoom, self.viewport_w / 2.0 * self.zoom,
+            - self.viewport_h / 2.0 * self.zoom, self.viewport_h / 2.0 * self.zoom,
+            -1.0, 1.0);
+        let instances = device
+            .create_buffer(self.instance_count,
+                           gfx::buffer::Role::Vertex,
+                           gfx::memory::Usage::Data,
+                           gfx::TRANSFER_DST).unwrap();
+        self.data.instance = instances;
+        self.data.matrix = Into::<[[f32; 4]; 4]>::into(ortho_matrix);
+
+        let mut encoder = pool.acquire_graphics_encoder();
+        encoder.copy_buffer(&upload, &self.data.instance,
+                            0, 0, upload.len()).unwrap();
+
+
+        let (cur_color, _) = self.views[frame.id()].clone();
+        self.data.out = cur_color;
+        use gfx_text;
+        let mut normal_text = gfx_text::new(factory.clone()).unwrap();
+        normal_text.draw(&mut encoder, &cur_color).unwrap();
+        encoder.clear(&self.data.out, [0.1, 0.2, 0.3, 1.0]);
+        encoder.draw(&self.slice, &self.pso, &self.data);
+        encoder.synced_flush(queue, &[&sync.rendering], &[], Some(&sync.frame_fence))
+               .expect("Could not flush encoder");
+    }
+
+    fn on_resize(&mut self, window_targets: support::WindowTargets<B::Resources>) {
+        self.views = window_targets.views;
+    }
+
+    fn update(&mut self, tick: f32, events: &mut sdl2::EventPump) {
+        // get a mouse state
+        let state = events.mouse_state();
+
+        // Create a set of pressed Keys.
+        let buttons = state.pressed_mouse_buttons().collect();
+
+        // Get the difference between the new and old sets.
+        let new_buttons = &buttons - &self.prev_buttons;
+        let old_buttons = &self.prev_buttons - &buttons;
+
+        let x = state.x() as f32 - self.viewport_w / 2.0;
+        let y = self.viewport_h / 2.0 - state.y() as f32;
+
+        // right mouse click
+        let right = &sdl2::mouse::MouseButton::Right;
+        if new_buttons.contains(right) {
+            println!("Mouse coord: {:?}, {:?}", x, y);
+            let picked_tile_id = self.tiles.tree.find(&cgmath::Vector2::new(x * self.zoom, y * self.zoom));
+            if self.cur_tile.is_some() {
+                self.tiles.tiles[self.cur_tile.unwrap()].is_selected = false;
+            }
+            if picked_tile_id.is_some() {
+                let sel_id = picked_tile_id.unwrap();
+                self.tiles.tiles[sel_id].is_selected = true;
+                println!("Sprite coords: {:?}", self.tiles.tiles[sel_id].position);
+                self.cur_tile = Some(sel_id);
+            }
+            println!("Clicked at tile: {:?}", picked_tile_id);
+        }
+        self.selection.update(x, y, &new_buttons, &old_buttons, &buttons);
+        self.miners.update(tick as f32, &self.tiles);
+
+        if self.selection.pressed {
+            self.tiles.update_selected(&self.selection);
+        }
+
+        // handle events
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit { .. } |
+                Event::KeyUp { keycode: Some(Keycode::Escape), .. } => {
+                    self.running = false;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Equals), .. } => {
+                    self.zoom -= 0.5;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Minus), .. } => {
+                    self.zoom += 0.5;
+                },
+                _ => {}
+            }
+        }
+    }
+
+    fn is_running(&self) -> bool {
+        self.running
+    }
+}
+
+pub fn main() {
+    use support::Application;
+    App::launch_simple(800, 600);
+}
---
+++ src/support/mod.rs
@@ -0,0 +1,177 @@
+use gfx::memory::Typed;
+use gfx::queue::GraphicsQueue;
+use gfx::{Adapter, Backend, CommandQueue, FrameSync, GraphicsCommandPool,
+          Swapchain, QueueType, WindowExt};
+use gfx::format::{Rgba8, DepthStencil};
+use clock_ticks;
+use env_logger;
+use gfx;
+use gfx_core;
+use gfx_device_gl;
+use gfx_window_sdl;
+use sdl2;
+use sdl2::gfx::primitives::DrawRenderer;
+use std;
+
+pub mod shade;
+
+pub type ColorFormat = gfx::format::Rgba8;
+
+pub type DepthFormat = gfx::format::DepthStencil;
+
+pub type BackbufferView<R: gfx::Resources> = (gfx::handle::RenderTargetView<R, ColorFormat>,
+                                              gfx::handle::DepthStencilView<R, DepthFormat>);
+
+pub struct WindowTargets<R: gfx::Resources> {
+    pub views: Vec<BackbufferView<R>>,
+    pub aspect_ratio: f32,
+}
+
+pub struct SyncPrimitives<R: gfx::Resources> {
+    /// Semaphore will be signalled once a frame is ready.
+    pub acquisition: gfx::handle::Semaphore<R>,
+    /// Indicates that rendering has been finished.
+    pub rendering: gfx::handle::Semaphore<R>,
+    /// Sync point to ensure no resources are in-use.
+    pub frame_fence: gfx::handle::Fence<R>,
+}
+
+fn run<A, B, S>((width, height): (u32, u32),
+                    mut surface: S,
+                    adapters: Vec<B::Adapter>,
+                    sdl_context: sdl2::Sdl)
+    where A: Sized + Application<B>,
+          B: Backend,
+          S: gfx_core::Surface<B>,
+          B::Device: shade::ShadeExt,
+{
+    use self::shade::ShadeExt;
+    use gfx::format::Formatted;
+    use gfx::traits::Device;
+    use gfx::texture;
+
+    // Init device, requesting (at least) one graphics queue with presentation support
+    let gfx_core::Gpu { mut device, mut graphics_queues, .. } =
+        adapters[0].open_with(|family, ty| ((ty.supports_graphics() && surface.supports_queue(&family)) as u32, QueueType::Graphics));
+    let mut queue = graphics_queues.pop().expect("Unable to find a graphics queue.");
+
+    let config = gfx_core::SwapchainConfig::new()
+                    .with_color::<ColorFormat>()
+                    .with_depth_stencil::<DepthFormat>();
+    let mut swap_chain = surface.build_swapchain(config, &queue);
+
+    let views =
+        swap_chain
+            .get_backbuffers()
+            .iter()
+            .map(|&(ref color, ref ds)| {
+                let color_desc = texture::RenderDesc {
+                    channel: ColorFormat::get_format().1,
+                    level: 0,
+                    layer: None,
+                };
+                let rtv = device.view_texture_as_render_target_raw(color, color_desc)
+                                 .unwrap();
+
+                let ds_desc = texture::DepthStencilDesc {
+                    level: 0,
+                    layer: None,
+                    flags: texture::DepthStencilFlags::empty(),
+                };
+                let dsv = device.view_texture_as_depth_stencil_raw(
+                                    ds.as_ref().unwrap(),
+                                    ds_desc)
+                                 .unwrap();
+
+                (Typed::new(rtv), Typed::new(dsv))
+            })
+            .collect();
+
+    let shader_backend = device.shader_backend();
+    let mut app = A::new(&mut device, &mut queue, shader_backend, WindowTargets {
+        views: views,
+        aspect_ratio: width as f32 / height as f32, //TODO
+    });
+
+    // TODO: For optimal performance we should use a ring-buffer
+    let sync = SyncPrimitives {
+        acquisition: device.create_semaphore(),
+        rendering: device.create_semaphore(),
+        frame_fence: device.create_fence(false),
+    };
+
+    let mut graphics_pool = queue.create_graphics_pool(1);
+    let mut events = sdl_context.event_pump().unwrap();
+
+    let mut frames = 0;
+    let mut start_ns = clock_ticks::precise_time_ns();
+    let mut prev_ns = start_ns;
+
+    while app.is_running() {
+        let now_ns = clock_ticks::precise_time_ns();
+        let tick_ns = now_ns - prev_ns;
+        let tick_s = (tick_ns as f64) / 1_000_000_000f64;
+
+        app.update(tick_s as f32, &mut events);
+
+        graphics_pool.reset();
+        let frame = swap_chain.acquire_frame(FrameSync::Semaphore(&sync.acquisition));
+        app.render(&mut device, (frame, &sync), &mut graphics_pool, &mut queue);
+        swap_chain.present(&mut queue, &[]);
+
+        device.wait_for_fences(&[&sync.frame_fence], gfx::WaitFor::All, 1_000_000);
+        queue.cleanup();
+
+        // calculate FPS
+        frames += 1;
+        let duration_ns = clock_ticks::precise_time_ns() - start_ns;
+        let duration_s = (duration_ns as f64) / 1_000_000_000f64;
+        let fps = (frames as f64) / duration_s;
+        if duration_s > 1.0 {
+            start_ns = clock_ticks::precise_time_ns();
+            frames = 0;
+            println!("Duration: {}, count: {}", duration_s, fps);
+        }
+        prev_ns = now_ns;
+    }
+}
+
+pub type DefaultBackend = gfx_device_gl::Backend;
+
+pub trait Application<B: Backend>: Sized {
+    fn is_running(&self) -> bool;
+    fn new(&mut B::Device, &mut GraphicsQueue<B>,
+           shade::Backend, WindowTargets<B::Resources>) -> Self;
+    fn update(&mut self, tick: f32, events: &mut sdl2::EventPump);
+    fn render(&mut self, device: &mut B::Device, frame: (gfx_core::Frame, &SyncPrimitives<B::Resources>),
+                     pool: &mut GraphicsCommandPool<B>, queue: &mut GraphicsQueue<B>);
+
+    fn on_resize(&mut self, WindowTargets<B::Resources>) {}
+    fn on_resize_ext(&mut self, _device: &mut B::Device, targets: WindowTargets<B::Resources>) {
+        self.on_resize(targets);
+    }
+
+    fn launch_simple(w: u32, h: u32) where Self: Application<DefaultBackend> {
+        env_logger::init().unwrap();
+        <Self as Application<DefaultBackend>>::launch_default(w, h)
+    }
+    fn launch_default(w: u32, h: u32)
+        where Self: Application<DefaultBackend>
+    {
+        use gfx_core::format::Formatted;
+
+        let sdl_context = sdl2::init().unwrap();
+        let video = sdl_context.video().unwrap();
+        // Request opengl core 3.3:
+        video.gl_attr().set_context_profile(sdl2::video::GLProfile::Core);
+        video.gl_attr().set_context_version(3, 3);
+        let builder = video.window("SDL Window", w, h);
+        let (window, _gl_context) = gfx_window_sdl::build(builder, Rgba8::get_format(), DepthStencil::get_format()).unwrap();
+        let mut window = gfx_window_sdl::Window::new(window);
+        let (mut surface, adapters) = window.get_surface_and_adapters();
+        //let mut canvas = window.raw().into_canvas();
+
+        let dim = (w, h);
+        run::<Self, _, _>(dim, surface, adapters, sdl_context)
+    }
+}
